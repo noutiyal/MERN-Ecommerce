d2b61287c35791e86572bf60444a682ab7faffd3
noutiyal <123543321+noutiyal@users.noreply.github.com>
2023-08-12 11:53:41 +0530

    dlet this

--- api/router/registerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { UserRegister } = require("../controllers/registerControllers");
-
-router.post("/UserRegister", UserRegister);
-
-module.exports = router;
